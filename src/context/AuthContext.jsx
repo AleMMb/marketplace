abdaf4a18bf024e76b937de7bdfd8c4f84fbfbf8
AleMMb <alejandramellab@gmail.com>
2023-06-03 03:57:50 -0400

    AuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -11,27 +11,6 @@ export const AuthContextProvider = ({children}) => {
     const [user, setUser] = useState({});
 
 
-    /*const logIn = async (userEmail, userPassword) => {
-
-      const users = await getUsersData()
-      console.log("JSON users registrados: ")
-      console.log(users)
-      const userDB = users.find(
-          (item) => item.email === userEmail && item.password === userPassword
-      );
-      if (userDB) {
-          console.log("usuario encontrado: ");
-          console.log(userDB);
-          setUser(userDB);
-      } else {
-          setUser(null)
-          // throw new Error("User not found") //generar error en consola si el usuario no se encuentra.
-          console.log("usuario no encontrado -----");
-      }
-      return userDB; //devuelve el usuario encontrado (se llama en AppLogIn)
-    }*/
-
-
   const data={
     usuario,
     setUsuario,
