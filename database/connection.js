dfdfa66853360fc7144d028dfc032c1f2d0cc2e8
AleMMb <alejandramellab@gmail.com>
2023-06-16 19:32:05 -0400

    _

--- database/connection.js
+++ database/connection.js
@@ -2,11 +2,7 @@ const { Pool } = require("pg")
 require('dotenv').config()
 
 const pool = new Pool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DB_NAME,
-    allowExitOnIdle: true,
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
 module.exports = pool
