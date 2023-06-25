6bf332fe45e9242fe8de9b4ed3566140604864a3
AleMMb <alejandramellab@gmail.com>
2023-06-25 13:22:26 -0400

    cors

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ const port = process.env.PORT || 3001
 
 
 app.listen(3000, console.log(`Server corriendo en el puerto ${port}`))
-app.use(cors({origin: true, credentials: true}))
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+    preflightContinue: true,
+  }))
 app.use(express.json())
 app.use(express.static('public'))
 app.use('/', userRoutes)
